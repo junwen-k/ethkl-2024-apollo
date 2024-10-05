2c94a1ee596900a5618a0354b61a72c8d4a53db9
junwen-k <40173716+junwen-k@users.noreply.github.com>
2024-10-05 14:56:32 +0800

    feat(web): update siteplan canvas

--- apps/web/src/components/layout/app-shell.tsx
+++ apps/web/src/components/layout/app-shell.tsx
@@ -10,7 +10,7 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 export const AppShell = ({ children }: { children: React.ReactNode }) => (
   <div className="bg-muted/40 flex min-h-screen w-full flex-col">
     <Sidebar />
-    <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+    <div className="flex min-h-screen flex-col sm:gap-4 sm:py-4 sm:pl-14">
       <header className="bg-background sticky top-0 z-30 flex h-14 items-center gap-4 border-b px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
         <Sheet>
           <SheetTrigger asChild>
@@ -46,7 +46,7 @@ export const AppShell = ({ children }: { children: React.ReactNode }) => (
           <ConnectButton />
         </div>
       </header>
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
     </div>
   </div>
 )
