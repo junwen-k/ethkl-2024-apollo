9ef78ed75054469ec2776d4ba72e312ced842ed8
junwen-k <40173716+junwen-k@users.noreply.github.com>
2024-10-13 00:05:36 +0800

    feat(contract): verify contract

--- apps/contract/hardhat.config.ts
+++ apps/contract/hardhat.config.ts
@@ -18,6 +18,22 @@ const config: HardhatUserConfig = {
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
+  etherscan: {
+    apiKey: {
+      // eslint-disable-next-line turbo/no-undeclared-env-vars
+      scrollSepolia: process.env.SCROLLSCAN_API_KEY as string,
+    },
+    customChains: [
+      {
+        network: 'scrollSepolia',
+        chainId: 534351,
+        urls: {
+          apiURL: 'https://api-sepolia.scrollscan.com/api',
+          browserURL: 'https://sepolia.scrollscan.com/',
+        },
+      },
+    ],
+  },
 }
 
 export default config
